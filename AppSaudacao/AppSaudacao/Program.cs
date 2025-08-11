f0a2875da54621c43b3a74b35a6637c4c3ff4233
Pedro Sarmento <pedrosarmento1412@gmail.com>
2025-08-11 19:28:32 -0300

    11/08

---
+++ AppSaudacao/AppSaudacao/Program.cs
@@ -0,0 +1,6 @@
+﻿string nome, saudacao;
+
+Console.WriteLine("Informe o seu nome ");
+nome = Console.ReadLine();
+saudacao = "Seja bem vindo " + nome + ", ao curso de P.O.O";
+Console.WriteLine(saudacao);
