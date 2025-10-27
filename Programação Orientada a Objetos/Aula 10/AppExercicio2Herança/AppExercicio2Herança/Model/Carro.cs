201890f2458daa645d386f85ce61d9934fce7872
Betoesu <pedrosarmento1412@gmail.com>
2025-10-27 20:35:20 -0300

    27/10

---
+++ Programação Orientada a Objetos/Aula 10/AppExercicio2Herança/AppExercicio2Herança/Model/Carro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppExercicio2Herança.Model
+{
+    public class Carro:Pessoa
+    {
+        private Int32 numeroPortas;
+        private string tipoCombustivel;
+        private string motorizacao;
+    }
+}
